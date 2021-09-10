b62a0bfa55170a3cefc90907c7fc51c54b062398
tuanpham123456 <31348703+tuanpham123456@users.noreply.github.com>
2021-09-10 18:43:30 +0700

    Folder Structure

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./styles.scss";
+
+Header.PropTypes = {};
+
+function Header(props) {
+  return {};
+}
+
+export default Header;
---
+++ src/feature/Todo/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./styles.scss";
+
+TodoFeature.PropTypes = {};
+
+function TodoFeature(props) {
+  return {};
+}
+
+export default TodoFeature;
